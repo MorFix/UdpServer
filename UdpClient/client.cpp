3cf0ea429e4d07a96ef33b2a98b199da2af30371
MorFix <yapan7@outlook.com>
2021-10-20 23:57:16 +0300

    implement UDP server-client

---
+++ UdpClient/client.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8181
+#define MAXLINE 1024
+
+using namespace std;
+
+// Driver code
+int main() {
+    int sockfd;
+    char* username = new char[MAXLINE / 2];
+    char* password = new char[MAXLINE / 2];
+    char message[MAXLINE];
+    struct sockaddr_in servaddr; 
+
+    // SOCK_DGRAM means UDP
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    cout << "Please enter username:\n";
+    cin >> username;
+
+    strncat(message, username, sizeof(char) * MAXLINE / 2);
+    strcat(message, "~");
+
+    cout << "Please enter password:\n";
+    cin >> password;
+    strncat(message, password, sizeof(char) * MAXLINE / 2);
+
+    sendto(sockfd, message, strlen(message), MSG_CONFIRM, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+    close(sockfd);
+
+    return 0;
+}
---
+++ UdpServer/server.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT    8181
+#define MAXLINE 1024
+
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char* hello = "Hello from server";
+    struct sockaddr_in serveraddr, clientaddr;
+
+    // SOCK_DGRAM means UDP.
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    memset(&clientaddr, 0, sizeof(clientaddr));
+
+    // Filling server information
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int len = sizeof(clientaddr), messageLength;
+    
+    // Looping for messages
+    while (true) {
+        messageLength = recvfrom(sockfd, (char*)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr*)&clientaddr, (socklen_t*)&len);
+        buffer[messageLength] = '\0';
+
+        char* username = new char[MAXLINE / 2];
+        char* password = new char[MAXLINE / 2];
+
+        int index = 0;
+        char currChar = buffer[0];
+
+        while (currChar != '~' && currChar != '\0') {
+            strncat(username, &currChar, sizeof(char));
+            currChar = buffer[++index];
+        }
+
+        currChar = buffer[++index];
+        while (currChar != '\0') {
+            strncat(password, &currChar, sizeof(char));
+            currChar = buffer[++index];
+        }
+        
+        if (strcmp(username, "mor") == 0 && strcmp(password, "cohen") == 0) {
+            std::cout << "OK!\n";
+        }
+        else {
+            std::cout << "WRONG!\n";
+        }
+    }
+    
+    return 0;
+}
